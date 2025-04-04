254dca7d31d82059430d85bbd63b5665bd41a951
Boda Rajesh <154888255+RajeshBodatech@users.noreply.github.com>
2025-04-04 16:42:54 +0530

    Update Login.jsx

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom"; // For redirecting after login
 import "./Login.css";
 
-const URL = "http://localhost:5000/api/auth/login";
+const URL = "https://surplus-food-management.onrender.com/api/auth/login";
 
 export const Login = () => {
   const [user, setUser] = useState({
